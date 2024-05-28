b09e98f959813228966b805bc490673aae8f5160
Sampreet Kishan Kumar <sampy.prithvi2@gmail.com>
2024-05-28 18:23:26 -0400

    Second commit: Added important comments to all the files

--- mysql/test1.sql
+++ mysql/test1.sql
@@ -1,11 +1,15 @@
+-- use the test_db that was created during the mysql initialisation 
 USE test_db; 
 
+-- then create a simple table
 CREATE TABLE employee (
     name varchar(255),
     age int,
     sex VARCHAR(10)
 );
 
+
+-- add some dummy values
 INSERT INTO employee(name, age, sex) values ("Test",100,'M'); 
 INSERT INTO employee(name, age, sex) values ("Test2",92,'F'); 
 INSERT INTO employee(name, age, sex) values ("Adam",19,'M'); 
